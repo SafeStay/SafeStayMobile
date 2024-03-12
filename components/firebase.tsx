e725d933197af005be8e9a73f272c1ac97977e3d
Hilda Väänänen <hilda.vaananen@gmail.com>
2024-03-12 10:31:36 +0200

    Crime data haettu ja välitetty reltime databaseen

--- components/firebase.tsx
+++ components/firebase.tsx
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { Text } from 'react-native';
+import { useEffect, useState } from "react";
+import { Crime } from "./CrimeFetch";
+import { getDatabase, ref, push, onValue, DataSnapshot, remove } from "firebase/database";
 
 const firebase: React.FC = () => {
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,6 +22,37 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const [streetCrimes, setStreetCrimes] = useState<Crime[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchStreetCrimes = async () => {
+      try {
+        const response = await fetch(`https://data.police.uk/api/crimes-street/all-crime?lat=51.509865&lng=-0.118092`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch street crimes');
+        }
+        const data: Crime[] = await response.json();
+
+        // Save data to Firebase Realtime Database
+        //const crimesRef = ref((database, '/streetCrimes'), streetCrimes);
+        await push(ref(database, '/streetCrimes'), streetCrimes);
+
+        setStreetCrimes(data);
+      } catch (error) {
+        console.error('Error fetching street crimes:', error);
+        setError((error as Error).message || 'An error occurred');
+      }
+    };
+
+    fetchStreetCrimes();
+  }, []);
+
+  //'51.509865', '-0.118092'
+
+
 
 return (
     <>
