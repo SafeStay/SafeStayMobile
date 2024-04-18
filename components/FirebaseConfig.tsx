e1cdcf1e6b29520ac00e680b67c5dbd5c734b6de
ArttuuS <arttu.saily@gmail.com>
2024-04-18 13:19:37 +0300

    modified firebaseconfig, added experimentalForceLongPolling true

--- components/FirebaseConfig.tsx
+++ components/FirebaseConfig.tsx
@@ -1,16 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-
+import { initializeApp } from "firebase/app";
+import { getFirestore, initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: `${process.env.FIREBASEAPIKEY}` ,
-    authDomain: "safestay-93c0d.firebaseapp.com",
-    databaseURL: "https://safestay-93c0d-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "safestay-93c0d",
-    storageBucket: "safestay-93c0d.appspot.com",
-    messagingSenderId: "47383716792",
-    appId: "1:47383716792:web:2bf84d57b08843f2e2254d"
+  apiKey: `${process.env.FIREBASEAPIKEY}`,
+  authDomain: "safestay-93c0d.firebaseapp.com",
+  databaseURL:
+    "https://safestay-93c0d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "safestay-93c0d",
+  storageBucket: "safestay-93c0d.appspot.com",
+  messagingSenderId: "47383716792",
+  appId: "1:47383716792:web:2bf84d57b08843f2e2254d",
 };
 
 const app = initializeApp(firebaseConfig);
-export const database = getFirestore(app);
+export const database = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
